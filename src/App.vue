30190e3a0dc059742eea25aed6e837597c7e76ef
tamaina <tamaina@hotmail.co.jp>
2023-05-09 02:40:04 +0900

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -7,51 +7,48 @@ const windows = ref<any[]>([1]);
 </script>
 
 <template>
-<div class="container">
-<div class="c1">
-  <button class="button" @click="items2.unshift(1)">add</button>
-  <div class="list">
-      <div v-for="item in items2" class="item">
+  <div class="container">
+    <div class="c1">
+      <button class="button" @click="items2.unshift(1)">add</button>
+      <div class="list">
+        <div v-for="item in items2" class="item">
           <p>あああ</p>
           <div class="body">
-              <p>いいい</p>
-              <div class="inner">
-                  <div class="inner1">
-                    <div class="inner2">
-                      <img src="./assets/image.webp" class="img" />
-                    </div>
-                  </div>
+            <p>いいい</p>
+            <div class="inner">
+              <div class="inner1">
+                <div class="inner2">
+                  <img src="./assets/image.webp" class="img" />
+                </div>
               </div>
+            </div>
           </div>
+        </div>
       </div>
-  </div>
-</div>
-<div class="c2">
-  <button class="button" @click="items.unshift(1)">add</button>
-  <div class="list">
-      <div v-for="item in items" class="item">
+    </div>
+    <div class="c2">
+      <button class="button" @click="items.unshift(1)">add</button>
+      <div class="list">
+        <div v-for="item in items" class="item">
           <p>あああ</p>
           <div class="body">
-              <p>いいい</p>
-              <div class="inner">
-                  <div class="inner1">
-                    <div class="inner2">
-                      <img src="./assets/image.webp" class="img" />
-                    </div>
-                  </div>
+            <p>いいい</p>
+            <div class="inner">
+              <div class="inner1">
+                <div class="inner2">
+                  <img src="./assets/image.webp" class="img" />
+                </div>
               </div>
+            </div>
           </div>
+        </div>
       </div>
+    </div>
+    <div class="c3">
+      <button @click="windows.push(1)">add window</button>
+    </div>
+    <div v-for="window in windows" class="window">あ</div>
   </div>
-</div>
-<div class="c3">
-  <button @click="windows.push(1)">add window</button>
-</div>
-<div v-for="window in windows" class="window">
-  あ
-</div>
-</div>
-
 </template>
 
 <style scoped>
@@ -62,66 +59,62 @@ const windows = ref<any[]>([1]);
   overflow-x: auto;
 }
 .c1 {
-    width: 330px;
-    min-width: 330px;
-    height: 100%;
-    background-color: #000;
-    color: #fff;
+  width: 330px;
+  min-width: 330px;
+  height: 100%;
+  background-color: #000;
+  color: #fff;
 }
 .c2 {
-    width: 330px;
-    min-width: 330px;
-    height: 50%;
-    background-color: #000;
-    color: #fff;
+  width: 330px;
+  min-width: 330px;
+  height: 50%;
+  background-color: #000;
+  color: #fff;
 }
 .button {
   height: 50px;
 }
 .list {
-    height: calc(100% - 50px);
-    background-color: #f0f;
-    color: #000;
-    overflow-y: auto;
-    container-type: size;
+  height: calc(100% - 50px);
+  background-color: #f0f;
+  color: #000;
+  overflow-y: auto;
+  container-type: size;
 }
 .item {
-    padding: 1em;
-    background-color: #000;
-    color: #fff;
+  padding: 1em;
+  background-color: #000;
+  color: #fff;
 }
 .body {
-    background-color: #0ff;
-    color: #000;
-    container-type: inline-size;
+  background-color: #0ff;
+  color: #000;
+  container-type: inline-size;
 }
 .inner {
   display: grid;
   grid-template-rows: 1fr;
   height: 100%;
-	min-height: 64px;
-	max-height: clamp(
-		64px,
-		50cqh,
-		min(360px, 50vh)
-	);
+  min-height: 64px;
+  max-height: clamp(64px, 50cqh, min(360px, 50vh));
   overflow: clip;
 }
 .inner1 {
   display: block;
   overflow: hidden;
-	width: 100%;
-	height: 100%;
+  width: 100%;
+  height: 100%;
 }
 .inner2 {
-	position: relative;
-	width: 100%;
-	height: 100%;
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
 .img {
-	display: block;
-	width: 100%;
-	height: 100%;
+  display: block;
+  width: 100%;
+  height: 100%;
   object-fit: contain;
 }
 
@@ -132,6 +125,7 @@ const windows = ref<any[]>([1]);
   width: 100px;
   height: 100px;
   background: #f00;
-  opacity: .1
+  opacity: 0.1;
+  container-type: size;
 }
 </style>
